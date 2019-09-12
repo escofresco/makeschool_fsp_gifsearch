9065bc229d13eadfded3729470dcea9632280f86
Jonasz Karwacki <jonaszakr@gmail.com>
2019-09-12 16:14:50 -0700

    Update index

--- app.py
+++ app.py
@@ -5,4 +5,8 @@ from flask import (Flask,
 app = Flask(__name__)
 
 @app.route('/')
-def
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
