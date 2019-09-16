66d170a58fa95307e2eea34bd512518b6e5e8d1d
Jonasz Karwacki <jonaszakr@gmail.com>
2019-09-15 18:21:36 -0700

    Add route that takes a query

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import (Flask,
                    render_template,
                    request,)
+from tenor import Tenor
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/search')
+def search():
+    partial_search = request.args.get('partial_search')
+    return Tenor().search(q=partial_search,
+                          contentfilter='high')
 
 if __name__ == '__main__':
     app.run(debug=True)
