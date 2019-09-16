2786c73e7cb726d3402dd3a30f5d18d82c5dc771
Jonasz Karwacki <jonaszakr@gmail.com>
2019-09-15 18:21:13 -0700

    Make simple Tenor api wrapper with search func

---
+++ tenor.py
@@ -0,0 +1,20 @@
+import json
+import requests
+from urllib import parse
+
+class Tenor(object):
+
+    def __init__(self, api_key='QA245EMO57HT', limit=10):
+        self.api_key = api_key
+        self.limit = limit
+        self.url_base = 'https://api.tenor.com/v1/'
+
+    def search(self, **params):
+        params['key'] = self.api_key
+        params['limit'] = self.limit
+        url = self.url_base+'search?'+parse.urlencode(params)
+        endpoint = ''.join(url)
+        res = requests.get(self.url)
+        if res.status_code == 200:
+            return json.loads(res.content)
+        return res.status_code
