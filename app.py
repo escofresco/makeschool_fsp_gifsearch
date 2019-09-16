25f575390b4230ac5b28b8d28c34e1e40e4f8442
Jonasz Karwacki <jonaszakr@gmail.com>
2019-09-16 15:01:48 -0700

    Return jsonify object from search endpoint

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
 from flask import (Flask,
+                   jsonify,
                    render_template,
                    request,)
+
 from tenor import Tenor
 
-app = Flask(__name__, static_url_path='/static')
+app = Flask(__name__, static_url_path='/static') # https://stackoverflow.com/questions/20646822/how-to-serve-static-files-in-flask
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',
+                           test_results=Tenor().search(q='big',
+                                                       contentfilter='high')['results'])
 
-@app.route('/search')
+@app.route('/search', methods=['POST'])
 def search():
-    partial_search = request.args.get('partial_search')
-    return Tenor().search(q=partial_search,
-                          contentfilter='high')
+    partial_search = request.args.get('search_input')
+    return jsonify(Tenor().search(q=partial_search,
+                                  contentfilter='high')['results'])
 
 if __name__ == '__main__':
     app.run(debug=True)
