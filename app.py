9a24827dcc5450ba676715390441952d98398d4a
Jonasz Karwacki <jonaszakr@gmail.com>
2019-09-19 16:27:50 -0700

    Add random quote route

--- app.py
+++ app.py
@@ -3,15 +3,27 @@ from flask import (Flask,
                    render_template,
                    request,
                    url_for,)
-
+from random import choice
 from tenor import Tenor
 
 app = Flask(__name__, static_url_path='/static') # https://stackoverflow.com/questions/20646822/how-to-serve-static-files-in-flask
 
+with open('quotes.json') as quotes:
+    random_quotes_json = json.load(quotes)
+    random_quotes_authors = list(random_quotes_json.keys())
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/random_error_messages')
+def random_error_messages():
+    if request.method == 'POST':
+        random_author = choice(random_quotes_authors)
+        return jsonify({
+            'author': random_author,
+            'quote': random_quotes_json[random_author]
+        })
 
 @app.route('/autocomplete', methods=['POST'])
 def autocomplete():
