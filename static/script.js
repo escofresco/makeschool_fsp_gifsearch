c3910c0556f6ddbc51c83f23c9b3898ccbfa75d9
Jonasz Karwacki <jonaszakr@gmail.com>
2019-09-19 15:37:11 -0700

    Add display handlers

--- static/script.js
+++ static/script.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
-    $('#search-form').submit(function(event) {
+    var searchInputElm = $('#searchInput');
+    var searchDropdownElm = $('#searchSuggestionDropdown');
+    var searchForm = $('#searchForm');
+    searchForm.submit(function(event) {
         event.preventDefault();
         $.ajax({
             type: 'POST',
             url: $(this).attr('action'),
             contentType: 'application/json;charset=UTF-8',
             data: JSON.stringify({
-                    'partial_search':$(this).find('input[name="search_input"]').val()
+                'partial_search':searchInputElm.val()
             }),
             success: function(data) {
                 if (data.length > 0) {
@@ -21,16 +24,61 @@ $(document).ready(function() {
         });
     });
 
+    searchInputElm.on('input', function() {
+        $.ajax({
+            type: 'POST',
+            url: '/autocomplete',
+            contentType: 'application/json;charset=UTF-8',
+            data: JSON.stringify({
+                'partial_search':searchInputElm.val()
+            }),
+            success: function(data) {
+                updateSuggestionMenu(data);
+            },
+            error: function(data) {
+                hideSuggestionMenu();
+            },
+        });
+    });
+
+    function suggestionDropdownItemClickHandler() {
+        // Update search bar's text to the selected text
+        searchInputElm.val($(this).text());
+
+        // Submit form with the selected text
+        searchForm.submit();
+    }
+
+    function updateSuggestionMenu(data) {
+        searchDropdownElm.html('');
+        data.forEach(function(elm, idx) {
+            var elmId = `item-${idx}-${elm}`
+            searchDropdownElm.append(`<a
+                                        class="dropdown-item"
+                                        id="${elmId}"
+                                        href="#">${elm}</a>`);
+        });
+        searchDropdownElm.addClass('show');
+        data.forEach(function(elm, idx) {
+            $(`#item-${idx}-${elm}`).click(suggestionDropdownItemClickHandler);
+        });
+    }
+
+    function hideSuggestionMenu() {
+        searchDropdownElm.removeClass('show');
+        searchDropdownElm.html('');
+    }
+
     function showGifs(tenorJson) {
         var columnContent = '';
         tenorJson.forEach(function(elm, idx) {
             columnContent += `<div class="row">
-                                <div class="col-6">
-                                    <div class="card" style="width: 18rem;">
-                                      <img src="${elm['media'][0]['gif']['url']}" class="card-img-top" alt="${elm['title']}">
-                                    </div>
-                                </div>
-                             </div>`
+                            <div class="col-6">
+                            <div class="card" style="width: 18rem;">
+                            <img src="${elm['media'][0]['gif']['url']}" class="card-img-top" alt="${elm['title']}">
+                            </div>
+                            </div>
+                            </div>`;
         });
         $('#gif-column').html(columnContent);
     }
